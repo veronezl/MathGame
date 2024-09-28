4278fbd8085332f8e75abe6d5ca1a833fe29c0b6
veronezl <lucasveronez.dev@gmail.com>
2024-09-28 19:48:07 -0300

    bug: resolvido bug da linha 63

--- MathGame/MainWindow.xaml.cs
+++ MathGame/MainWindow.xaml.cs
@@ -59,10 +59,14 @@ namespace MathGame
 
                 foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
                 {
-                    int index = random.Next(animalEmoji.Count);
-                    string nextEmoji = animalEmoji[index];
-                    textBlock.Text = nextEmoji;
-                    animalEmoji.RemoveAt(index);
+                    if (textBlock.Name != "timeTextBlock")
+                    {
+                        textBlock.Visibility = Visibility.Visible;
+                        int index = random.Next(animalEmoji.Count);
+                        string nextEmoji = animalEmoji[index];
+                        textBlock.Text = nextEmoji;
+                        animalEmoji.RemoveAt(index);
+                    }
                 }
             }
 
